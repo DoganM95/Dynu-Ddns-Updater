1ae413239ce5c54425c1d3b46633f5afd481a46e
DoganM95 <doganmermertas@hotmail.de>
2023-10-10 02:58:02 +0200

    add service rotation for higher polling rate if needed

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,19 @@
 #!/bin/sh
 
 PREVIOUS_IPV4=""
+SERVICES="http://ipinfo.io/ip" # Add services into existing string, separated by space
+index=0
 
 while true; do
-    CURRENT_IPV4=$(wget -qO- "https://ipinfo.io/ip")
-    # CURRENT_IPV6=$(wget -qO- "https://ip6.me/api/")
-    if [ "$CURRENT_IPV4" != "$PREVIOUS_IPV4" ]; then
-        echo "New IPV4: $CURRENT_IPV4"
-        echo # Add a line break
-        wget -qO- "http://api.dynu.com/nic/update?myip=${CURRENT_IPV4}&username=${DYNU_USERNAME}&password=${DYNU_PASSWORD}"
-        PREVIOUS_IPV4=$CURRENT_IPV4
-    fi
-    sleep $POLLING_INTERVAL
+    for SERVICE in $SERVICES; do
+        CURRENT_IPV4=$(curl -s "$SERVICE")
+        # CURRENT_IPV6=$(wget -qO- "https://ip6.me/api/")
+        if [ "$CURRENT_IPV4" != "$PREVIOUS_IPV4" ]; then
+            echo "New IPV4: $CURRENT_IPV4"
+            echo # Add a line break
+            wget -qO- "http://api.dynu.com/nic/update?myip=${CURRENT_IPV4}&username=${DYNU_USERNAME}&password=${DYNU_PASSWORD}"
+            PREVIOUS_IPV4=$CURRENT_IPV4
+        fi
+        sleep $POLLING_INTERVAL
+    done
 done
