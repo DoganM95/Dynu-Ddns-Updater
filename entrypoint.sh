8906125e1eb5ad6b1523fe4540964a1184c7b606
Dogan <doganmermertas@hotmail.de>
2024-03-28 09:10:04 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,11 +9,14 @@ while true; do
         CURRENT_IPV4=$(curl -s "$SERVICE")
         # CURRENT_IPV6=$(wget -qO- "https://ip6.me/api/")
         if [ "$CURRENT_IPV4" != "$PREVIOUS_IPV4" ]; then
-            echo "New IPV4: $CURRENT_IPV4"
+            echo "Updating IPV4 from $PREVIOUS_IPV4 to $CURRENT_IPV4"
             echo # Add a line break
             wget -qO- "http://api.dynu.com/nic/update?myip=${CURRENT_IPV4}&hostnme=${DYNU_HOSTNAME}&username=${DYNU_USERNAME}&password=${DYNU_PASSWORD}"
             PREVIOUS_IPV4=$CURRENT_IPV4
         fi
         sleep $POLLING_INTERVAL
+        echo "CURRENT_IPV4: ${CURRENT_IPV4}"
+        echo "DYNU_HOSTNAME: ${DYNU_HOSTNAME}"
+        echo 
     done
 done
